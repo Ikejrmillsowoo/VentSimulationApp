b167bf687f9b82cff09c334fa845f2c6f7e63e7c
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2025-09-04 11:27:52 -0400

    dto for simulated response

--- src/main/java/com/ventsim/ventsim/dto/SimulateRequest.java
+++ src/main/java/com/ventsim/ventsim/dto/SimulateRequest.java
@@ -1,4 +1,19 @@
 package com.ventsim.ventsim.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class SimulateRequest {
+    @NotBlank
+    public String stateId;
+    @NotBlank public String scenario;        // can switch if desired
+    @NotBlank public String mode;
+    @NotNull
+    public Integer tidalVolume;    // mL
+    @NotNull  public Integer respiratoryRate;
+    @NotNull  public Integer peep;
+    @NotNull  public Integer fio2;           // %
+    public Double weight;                    // kg (optional override)
+    public Integer inspiratoryPressure;      // cmH2O (PC)
+    public Integer supportPressure;          // cmH2O (PSV)
 }
