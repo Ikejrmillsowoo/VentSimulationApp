7a463cf605d41c49b4e87e2c877a858c9348e5f6
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2025-09-04 23:15:46 -0400

    updated controller and added handler

--- src/main/java/com/ventsim/ventsim/controller/SimulationController.java
+++ src/main/java/com/ventsim/ventsim/controller/SimulationController.java
@@ -1,38 +1,56 @@
 package com.ventsim.ventsim.controller;
 
+import com.ventsim.ventsim.dto.InitRequest;
+import com.ventsim.ventsim.dto.InitResponse;
+import com.ventsim.ventsim.dto.SimulateRequest;
+import com.ventsim.ventsim.dto.SimulateResponse;
 import com.ventsim.ventsim.model.SimulationRequest;
 import com.ventsim.ventsim.model.SimulationResponse;
 //import com.ventsim.ventsim.service.PreliminarySettingsService;
 import com.ventsim.ventsim.service.SimulationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/simulate")
 public class SimulationController {
-    @Autowired
-    private SimulationService simulationService;
+    private final SimulationService service;
+    public SimulationController(SimulationService service) { this.service = service; }
+
+    @PostMapping(value="/init", consumes= MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public InitResponse init(@Valid @RequestBody InitRequest request) {
+        return service.init(request);
+    }
+
+    @PostMapping(value="/simulate", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public SimulateResponse simulate(@Valid @RequestBody SimulateRequest request) {
+        return service.simulate(request);
+    }
+
+    @GetMapping("/health")
+    public String health() { return "OK"; }
+//    @Autowired
+//    private SimulationService simulationService;
 //    @Autowired
 //    private PreliminarySettingsService preliminarySettingsService;
 
-    @PostMapping
-    public ResponseEntity<SimulationResponse> runSimulation(@RequestBody SimulationRequest request) {
-        SimulationResponse response = simulationService.simulate(request);
-        return ResponseEntity.ok(response);
-    }
+//    @PostMapping
+//    public ResponseEntity<SimulationResponse> runSimulation(@RequestBody SimulationRequest request) {
+//        SimulationResponse response = simulationService.simulate(request);
+//        return ResponseEntity.ok(response);
+//    }
 
     // post mapping for conditional patient set up
-    @PostMapping("/data")
-    public int getData(@RequestBody SimulationRequest request){
-        int weight = request.getWeight();
-        System.out.println(weight);
-        int data = request.getTargetVolume();
-        return data;
-    }
+//    @PostMapping("/data")
+//    public int getData(@RequestBody SimulationRequest request){
+//        int weight = request.getWeight();
+//        System.out.println(weight);
+//        int data = request.getTargetVolume();
+//        return data;
+//    }
     // simulation start with enter patient type pick from list
     //enters patient weight
 
--- src/main/java/com/ventsim/ventsim/support/RestExceptionHandler.java
+++ src/main/java/com/ventsim/ventsim/support/RestExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.ventsim.ventsim.support;
 
+import com.ventsim.ventsim.service.SimulationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+@ControllerAdvice
 public class RestExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ErrorPayload("validation_error", msg)
+        );
+    }
+
+    record ErrorPayload(String code, String message) {}
+
 }
