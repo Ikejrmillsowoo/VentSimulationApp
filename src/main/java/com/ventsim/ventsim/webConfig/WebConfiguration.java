c73b75252bd180ddc6d2336440b453347a79f105
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2026-05-04 20:01:45 -0400

    Update CORS configuration to allow new frontend origin

--- src/main/java/com/ventsim/ventsim/webConfig/WebConfiguration.java
+++ src/main/java/com/ventsim/ventsim/webConfig/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.0:3000", "https://miniature-space-happiness-46w5q5vxg5r24w6-3001.app.github.dev", "https://miniature-space-happiness-46w5q5vxg5r24w6-3000.app.github.dev") // adjust to your frontend port
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.0:3000", "https://ventsimulationapp.onrender.com") // adjust to your frontend port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
